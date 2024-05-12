d8f9154b01794236ea2c7a6f3846b20c4bf5a326
DimaSalem <demamohamad1998@gmail.com>
2024-05-12 12:52:36 +0300

    solve Lab12

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
+# Movies_Library - V.0.1 
+  **Author Name**: Dima Salem
+  
+![wrrc](./imgs/wrrc2.jpg)
+## Overview 
+This project is an Express.js API for accessing movie data. It lets users fetch trending movies and search for movies by title. The app connects to The Movie Database API to get movie information.
+
+## Getting Started
+- Using Nodejs (RunTime enviroment )
+- Install the cors Package
+   - `npm install cors`
+- Install nodemon Package 
+   - `npm i nodemon`
+- install axios and dotenv
+    - `npm install axios dotenv`
+## New Project Features   
+- Trending Movies Endpoint:
+
+    Endpoint: `/trending`.
+
+    Description: Retrieves a list of trending movies from the TMDB API and returns the data as JSON.
+- Search Movies Endpoint:
+
+     Endpoint: `/search`. 
+
+    Description: Allows users to search for movies based on their titles. Users can provide a query parameter (movieName) to search for specific movies. The endpoint queries the TMDB API and returns the search results as JSON.
+
 # Movies-Library v.0.0
 **Author Name**: Dima Salem
 
@@ -20,5 +47,6 @@
 3. Error Handling: The app handel two type of error 
     - 500: "Internal Server Error".
     - 404: "Not Found".
+
 
 
--- server.js
+++ server.js
@@ -1,30 +1,100 @@
+'use strict'
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3002
+const axios = require('axios');
+const cors = require('cors');
+app.use(cors());
+
+const port = process.env.PORT;;
+const apiKey = process.env.API_KEY;
+
 const movieData = require('./Movie_Data/data.json');
 
-
 //routs
+app.get('/trending', trendingHandler);
+app.get("/search", searchHandler);
+app.get("/popular", popularHandler);
+app.get("/topRated", topRatedHandler);
+
+
 app.get('/', homePageHandler);
 app.get('/favorite', favoritePgeHandler);
 
 
 //functions
+//Lab12
+function trendingHandler(req, res) {
+    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&language=en-US`;
+    axios.get(url)
+        .then(result => {
+            let trendingMovie = result.data.results.map(movie => {
+                return new Movie(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview)
+            })
+            res.json(trendingMovie)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+function searchHandler(req, res) {
+    let movieName = req.query.name;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieName}&page=2`;
+
+    axios.get(url)
+        .then(result => {
+            res.json(result.data.results);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+function popularHandler(req, res) {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+
+    axios.get(url)
+        .then(result => {
+            res.json(result.data.results);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+function topRatedHandler(req, res) {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`;
+
+    axios.get(url)
+        .then(result => {
+            res.json(result.data.results);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
+
+
+
+//Lab11
 function homePageHandler(req, res) {
     let result;
-    result = new Movie(movieData.title, movieData.poster_path, movieData.overview);
-    res.json(result);
+    //result = new Movie(movieData.title, movieData.poster_path, movieData.overview);
+    //res.json(result);
 }
 function favoritePgeHandler(req, res) {
     res.send("Welcome to Favorite Page");
 }
 
 //constructor
-function Movie(title, poster_path, overview) {
+function Movie(id, title, releaseDate, postarPath, overview) {
+    this.id = id;
     this.title = title;
-    this.poster_path = poster_path;
+    this.release_date = releaseDate;
+    this.postar_path = postarPath;
     this.overview = overview;
 }
+
 
 //error handler 
 function handleNotFoundError(req, res) {
